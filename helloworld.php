4cea317e7b27180f7f0f43e5411bb72ff20111a8
RodrigoRS11 <rodrigoogaitnas@gmail.com>
2023-08-25 15:25:46 -0300

    Mudando para Js e Python

--- helloworld.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "Hello,World!";
-echo "Hello,World2!";
-echo "Hellow, World3!";
-
-echo "testando o commit";
-echo "testando o commit";
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    echo "Hellowworld!";
-    echo "Hellowworld!";
-?>
